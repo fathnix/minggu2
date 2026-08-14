5084ed1c0bef0679b0135f7fe0da55dc3cb42726
fathnix <mhmd.fatih2106@gmail.com>
2026-08-14 14:56:24 +0700

    buat api-crud

---
+++ api-crud/app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        // Tadi kamu nulisnya Post::All() pakai A besar, biasakan pakai a kecil
+        return response()->json(Post::all(), 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'content' => 'required'
+        ]);
+
+        // Tadi kodenya berhenti di validate aja, data barunya belum disuruh simpan!
+        // Ini baris yang bikin fungsinya jalan:
+        $post = Post::create($request->all());
+        
+        return response()->json($post, 201);
+    }
+}
